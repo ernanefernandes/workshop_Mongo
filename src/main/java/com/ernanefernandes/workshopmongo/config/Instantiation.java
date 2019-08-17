1fa698c315ae42dae462d4d9b3c50f1ca6516adf
ernanefernandes <ernane.fernandes@gmail.com>
2019-08-17 15:40:45 -0300

    configurando

---
+++ src/main/java/com/ernanefernandes/workshopmongo/config/Instantiation.java
@@ -0,0 +1,28 @@
+package com.ernanefernandes.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.ernanefernandes.workshopmongo.domain.User;
+import com.ernanefernandes.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String ... arg0) throws Exception {
+	 userRepository.deleteAll();
+	 User maria = new User(null, "Maria Brown", "maria@gmail.com");
+	 User alex = new User(null, "Alex Green", "alex@gmail.com");
+	 User bob = new User(null, "Bob Grey", "bob@gmail.com");
+	 
+	 userRepository.saveAll(Arrays.asList(maria,alex,bob));
+	}
+	
+}
